2698713a026455a06f311d4700768e5e8c7e430f
hang0429 <90622507+hang0429@users.noreply.github.com>
2021-11-15 10:25:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+ZZZ=package/lean/default-settings/files/zzz-default-settings
+
+#设置密码admin
+sed -i /$1$0CVWiweD$4Xsq83ZIZtPJe8PVLfqJH0:16357:0:99999:7:::/d $ZZZ
+
+
+sed -i s/OpenWrt /by WX&QQ:187387176 compiled in $(TZ=UTC-8 date +%Y.%m.%d) @ OpenWrt /g $ZZZ
+
+#修改默认主题为argon
+sed -i s/luci-theme-bootstrap/luci-theme-argon/g feeds/luci/collections/luci/Makefile
